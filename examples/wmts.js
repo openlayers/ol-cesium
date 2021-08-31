19ab89f418830db1b2c2bfc69a7a337d862cfc0e
mulfvik <ulfvik.mikael@outlook.com>
2021-08-31 14:58:07 +0200

    Changes service in wmts example

--- examples/wmts.js
+++ examples/wmts.js
@@ -34,12 +34,12 @@ const map = new Map({
     new TileLayer({
       opacity: 0.7,
       source: new WMTS({
-        attributions: 'Tiles © <a href="https://services.arcgisonline.com/arcgis/rest/' +
-            'services/Demographics/USA_Population_Density/MapServer/">ArcGIS</a>',
-        url: 'https://services.arcgisonline.com/arcgis/rest/' +
-            'services/Demographics/USA_Population_Density/MapServer/WMTS/',
-        layer: '0',
-        matrixSet: 'EPSG:3857',
+        attributions: 'Tiles © <a href="https://sampleserver6.arcgisonline.com/arcgis/rest/' +
+            'services/WorldTimeZones/MapServer/WMTS/">ArcGIS</a>',
+        url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/' +
+            'services/WorldTimeZones/MapServer/WMTS/',
+        layer: 'WorldTimeZones',
+        matrixSet: 'GoogleMapsCompatible',
         format: 'image/png',
         projection,
         tileGrid: new WMTSTileGrid({
