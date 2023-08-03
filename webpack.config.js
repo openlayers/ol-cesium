95990290d079756caa2b5b95f480d67b4a468413
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2023-08-03 12:54:49 +0200

    Don't lint webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 // eslint-disable-next-line
-const {merge} = require('webpack-merge');
-const commons = require('./buildtools/webpack.commons.js');
+import {merge} from 'webpack-merge';
+import commons from './buildtools/webpack.commons.js';
+import devConfig from './buildtools/webpack.dev.js';
+import prodConfig from './buildtools/webpack.prod.js';
+import examplesConfig from './buildtools/webpack.examples.js';
+import libraryConfig from './buildtools/webpack.library.js';
+import umdConfig from './buildtools/webpack.umd.js';
+
 
 let config = commons.config;
 
 const nodeEnv = process.env['NODE_ENV'] || 'development';
 switch (nodeEnv) {
   case 'development':
-    config = merge(config, require('./buildtools/webpack.dev.js'));
+    config = merge(config, devConfig);
     break;
   case 'production':
-    config = merge(config, require('./buildtools/webpack.prod.js'));
+    config = merge(config, prodConfig);
     break;
   default:
     console.log(`The 'NODE_ENV' environement variable is set to an invalid value: ${process.env.NODE_ENV}.`);
@@ -19,17 +25,17 @@ switch (nodeEnv) {
 
 switch (process.env.TARGET) {
   case 'examples':
-    config = merge(config, require('./buildtools/webpack.examples.js'));
+    config = merge(config, examplesConfig);
     break;
   case 'library':
-    config = merge(config, require('./buildtools/webpack.library.js'));
+    config = merge(config, libraryConfig);
     break;
   case 'umd':
-    config = merge(config, require('./buildtools/webpack.umd.js'));
+    config = merge(config, umdConfig);
     break;
   default:
-    console.log(`The 'TARGET' environement variable is set to an invalid value: ${process.env.TARGET}.`);
-    process.exit(2);
+    console.log(`The 'TARGET' environement variable is set to an invalid value: ${process.env.TARGET}. Using library mode.`);
+    config = merge(config, libraryConfig);
 }
 
-module.exports = config;
+export default config;
