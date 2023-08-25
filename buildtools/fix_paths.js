fb79927c71904b8228d83a34dbe241bca89e5e56
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2023-08-25 16:12:02 +0200

    Blindly do some magics with extensions before publishing olcs

---
+++ buildtools/fix_paths.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+
+function handleFile(filePath) {
+  fs.readFile(filePath, 'utf-8', (error, content) => {
+    if (error) {
+      console.error('Error reading the file:', error);
+      return;
+    }
+    const split = content.split('\n');
+    let changed = false;
+    const newSplit = split.map((line) => {
+      if (!line.startsWith('import') || line.endsWith(".js';") || line.includes("from 'cesium")) {
+        return line;
+      }
+      let newline = undefined;
+      if (line.endsWith(".ts';")) {
+        newline = line.replace(".ts';", ".js';");
+      } else if (line.includes("from './")) {
+        newline = line.replace("';", ".js';");
+      } else {
+        console.error('XX', filePath, line);
+        throw new Error('pb', line);
+      }
+      console.log('changed:', line, '->', newline);
+      changed = true;
+      return newline;
+    });
+    if (changed) {
+      fs.writeFileSync(filePath, newSplit.join('\n'), {
+        encoding: 'utf-8'
+      });
+      console.log('wrote', filePath);
+    }
+  });
+}
+
+function listFilesRecursively(dir) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+
+    const stat = fs.statSync(filePath);
+
+    if (stat.isFile()) {
+      handleFile(filePath);
+    } else if (stat.isDirectory()) {
+      listFilesRecursively(filePath);
+    }
+  });
+}
+
+const startingDirectory = process.argv[2];
+listFilesRecursively(startingDirectory);
