41ba736004c07d7192c4fa68fbba22e60387a08d
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-12-07 09:10:09 +0100

    feat(code-sandbox): enable print example in cs

--- examples/print.js
+++ examples/print.js
@@ -8,6 +8,7 @@ import olMap from 'ol/Map.js';
 import {OLCS_ION_TOKEN} from './_common.js';
 
 
+const Cesium = window.Cesium;
 Cesium.Ion.defaultAccessToken = OLCS_ION_TOKEN;
 const ol2d = new olMap({
   layers: [
@@ -20,7 +21,7 @@ const ol2d = new olMap({
       collapsible: false
     }
   }),
-  target: 'map',
+  target: 'mapCesium',
   view: new olView({
     center: transform([25, 20], 'EPSG:4326', 'EPSG:3857'),
     zoom: 3
@@ -63,5 +64,10 @@ window['takeScreenshot'] = async function() {
   img.src = value;
   img.width = r.width / (canvas.width / canvas.clientWidth);
   img.height = r.height / (canvas.height / canvas.clientHeight);
-  document.body.append(img);
+  document.querySelector("#screenshots").append(img);
 };
+
+//##REMOVE## Keep this tag, split code here for code sandbox
+
+import {initCodeSandbox} from './_code-sandbox.js';
+initCodeSandbox('./print.js');
